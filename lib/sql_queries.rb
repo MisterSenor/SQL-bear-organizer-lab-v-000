774aec2e04ff466aff54f49562e2ef2b7b025e88
Nathan  Visak <nathan.visak@gmail.com>
2018-07-02 19:43:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, MAX(color) FROM bears;"
+  "SELECT color, COUNT(color) FROM bears ORDER By COUNT(color) LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -27,5 +27,4 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears WHERE name = 'nil';"
 end
